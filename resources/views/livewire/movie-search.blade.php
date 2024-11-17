b7f61186c9f7f770a8cfef3dbffca640752337ed
Tiyiselani Mabunda <tiyiselaadvo@gmail.com>
2024-11-18 01:18:30 +0200

    Update movie-search.blade.php

--- resources/views/livewire/movie-search.blade.php
+++ resources/views/livewire/movie-search.blade.php
@@ -1,3 +1,13 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <!-- Search Form -->
+    <div class="flex items-center space-x-2">
+        <input type="text" class="border p-2 flex-1" placeholder="Search for a movie..." >
+        <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600" >
+            Search
+        </button>
+    </div>
+
+
+    <!-- Search Results -->
+    
 </div>
