0e3b039bd527b14e1eda90621c9c77bf6b20ba80
Tiyiselani Mabunda <tiyiselaadvo@gmail.com>
2024-11-18 01:04:32 +0200

    add livewire component

---
+++ app/Livewire/MovieSearch.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class MovieSearch extends Component
+{
+    public function render()
+    {
+        return view('livewire.movie-search');
+    }
+}
