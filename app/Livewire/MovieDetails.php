e24940c106dfd5572431714cde106a7b5dab5e1e
Tiyiselani Mabunda <tiyiselaadvo@gmail.com>
2024-11-18 02:41:00 +0200

    add livewire component MovieDatails
    to display movie details

---
+++ app/Livewire/MovieDetails.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class MovieDetails extends Component
+{
+    
+    public function render()
+    {
+        return view('livewire.movie-details');
+    }
+}
