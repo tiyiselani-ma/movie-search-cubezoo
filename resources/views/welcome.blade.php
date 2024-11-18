c9cdded65899cd2854f8cf688ee3bfa67300cf63
Tiyiselani Mabunda <tiyiselaadvo@gmail.com>
2024-11-18 04:30:54 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,17 @@
 <x-guest-layout>
-    <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
-        <img id="background" class="absolute -left-20 top-0 max-w-[877px]"
-            src="https://laravel.com/assets/img/welcome/background.svg" alt="Laravel background" />
-        <div
-            class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
-            <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                <div class="container mx-auto mt-10">
-                    <h1 class="text-2xl font-bold mb-5">Search Movies</h1>
-                    <livewire:movie-search />
-                </div>
+    <div class=" min-h-screen ">
+        <div class=" h-72 flex justify-center items-center w-full">
+            <div class=" max-w-5xl ">
+                <h1 class=" text-5xl text-center uppercase font-light">The most convient way to search for your favourate movies</h1>
             </div>
         </div>
+        <div class="container mx-auto mt-10">
+            
+            <div class=" max-w-3xl mx-auto">
+                <h1 class="text-2xl font-bold mb-5">Search Movies</h1>
+                <livewire:movie-search />
+            </div>
+        </div>
+
     </div>
 </x-guest-layout>
