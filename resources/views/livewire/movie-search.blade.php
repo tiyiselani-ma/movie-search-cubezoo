16dc786ce77e6495dfc64a5c66db40c7f1967b22
Tiyiselani Mabunda <tiyiselaadvo@gmail.com>
2024-11-18 02:01:40 +0200

    Update movie-search.blade.php

--- resources/views/livewire/movie-search.blade.php
+++ resources/views/livewire/movie-search.blade.php
@@ -1,8 +1,11 @@
 <div>
     <!-- Search Form -->
     <div class="flex items-center space-x-2">
-        <input type="text" class="border p-2 flex-1" placeholder="Search for a movie..." >
-        <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600" >
+        <input type="text" class="border p-2 flex-1 text-black" 
+        placeholder="Search for a movie..."
+        wire:model.debounce.500ms="input"
+        wire:model="input" >
+        <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600" wire:click="searchMovies" >
             Search
         </button>
     </div>
