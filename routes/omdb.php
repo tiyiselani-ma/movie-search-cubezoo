bbde6cd353bf2a572b40d13e09affde37062b525
Tiyiselani Mabunda <tiyiselaadvo@gmail.com>
2024-11-18 23:00:22 +0200

    Update omdb.php
    add trending route

--- routes/omdb.php
+++ routes/omdb.php
@@ -1,6 +1,9 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use App\Livewire\TrendingMovies;
 
 Route::get('/movie/{imdbID}', function ($imdbID) {
     return view('movie-details', ['imdbID' => $imdbID]);
 })->name('movie.details');
+
+Route::get('/trending', TrendingMovies::class)->name('trending.movies');
