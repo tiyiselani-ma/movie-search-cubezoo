03be3aa16c35f13ac7be0baa5ceb7daafd2362ae
Tiyiselani Mabunda <tiyiselaadvo@gmail.com>
2024-11-18 03:33:12 +0200

    Update MovieDetails.php

--- app/Livewire/MovieDetails.php
+++ app/Livewire/MovieDetails.php
@@ -3,10 +3,34 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Http;
 
 class MovieDetails extends Component
 {
-    
+    public $imdbID;
+    public $movie;
+
+    public function mount($imdbID)
+    {
+        $this->imdbID = $imdbID;
+        $this->fetchMovieDetails();
+    }
+
+    public function fetchMovieDetails()
+    {
+       
+        $response = Http::get("http://www.omdbapi.com/", [
+            'apikey' => config('services.omdb.api_key'),
+            'i' => $this->imdbID,
+        ]);
+
+        if ($response->successful()) {
+            $this->movie = $response->json();
+        } else {
+            $this->movie = null;
+        }
+    }
+
     public function render()
     {
         return view('livewire.movie-details');
