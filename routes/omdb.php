6260d95fa48c5ec7d5af7656e696a3df0fe83807
Tiyiselani Mabunda <tiyiselaadvo@gmail.com>
2024-11-18 03:27:30 +0200

    Update omdb.php

--- routes/omdb.php
+++ routes/omdb.php
@@ -1,1 +1,6 @@
 <?php
+use Illuminate\Support\Facades\Route;
+
+Route::get('/movie/{imdbID}', function ($imdbID) {
+    return view('movie-details', ['imdbID' => $imdbID]);
+})->name('movie.details');
