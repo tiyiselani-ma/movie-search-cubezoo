c09d1f5cfca3f3b4d072e8b7749d6f3a776c7bfd
Tiyiselani Mabunda <tiyiselaadvo@gmail.com>
2024-11-18 22:59:32 +0200

    Update movie-search.blade.php

--- resources/views/livewire/movie-search.blade.php
+++ resources/views/livewire/movie-search.blade.php
@@ -4,9 +4,8 @@
         <input  type="text" 
                 class="border p-2 flex-1 text-black" 
                 placeholder="Search for a movie..."
-                wire:model.debounce.500ms="input" 
-                wire:keydown.debounce.500ms="searchMovies" 
-                wire:model="input">
+                wire:model.debounce.0ms="input" 
+        >
                 
         <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600" wire:click="searchMovies">
             Search
