59066eedca8f43e9bd85b7aa151413cd12e2e6e0
Tiyiselani Mabunda <tiyiselaadvo@gmail.com>
2024-11-18 03:29:52 +0200

    Update movie-details.blade.php

--- resources/views/livewire/movie-details.blade.php
+++ resources/views/livewire/movie-details.blade.php
@@ -1,3 +1,21 @@
-<div>
-    {{-- In work, do what you enjoy. --}}
+<div class="container mx-auto p-6">
+    @if ($movie)
+        <h1 class="text-2xl font-bold">{{ $movie['Title'] }}</h1>
+        <p> {{ $movie['Year'] }}</p>
+        <p> {{ $movie['Rated'] }}</p>
+        <p> {{ $movie['Released'] }}</p>
+        <p> {{ $movie['Runtime'] }}</p>
+        <p> {{ $movie['Genre'] }}</p>
+        <p> {{ $movie['Director'] }}</p>
+        <p> {{ $movie['Actors'] }}</p>
+        <p> {{ $movie['Plot'] }}</p>
+        <p> {{ $movie['imdbRating'] }}</p>
+
+        
+       <img src="{{ $movie['Poster'] }}" alt="Movie Poster" class="mt-4">
+     
+   
+    @endif
+
+
 </div>
