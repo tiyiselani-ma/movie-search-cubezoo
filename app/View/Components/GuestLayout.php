bf6d2746348756f497c4b081ca086ec07abba134
Tiyiselani Mabunda <tiyiselaadvo@gmail.com>
2024-11-18 03:24:20 +0200

    move component

---
+++ app/View/Components/GuestLayout.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\View\Components;
+
+
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class GuestLayout extends Component
+{
+   
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render()
+    {
+        return view('layouts.guest-layout');
+    }
+}
+
+
