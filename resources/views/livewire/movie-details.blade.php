e24f807955414a259346a928a6276120d80f6141
Tiyiselani Mabunda <tiyiselaadvo@gmail.com>
2024-11-18 03:49:05 +0200

    Update movie-details.blade.php

--- resources/views/livewire/movie-details.blade.php
+++ resources/views/livewire/movie-details.blade.php
@@ -1,21 +1,22 @@
 <div class="container mx-auto p-6">
     @if ($movie)
         <h1 class="text-2xl font-bold">{{ $movie['Title'] }}</h1>
-        <p> {{ $movie['Year'] }}</p>
-        <p> {{ $movie['Rated'] }}</p>
-        <p> {{ $movie['Released'] }}</p>
-        <p> {{ $movie['Runtime'] }}</p>
-        <p> {{ $movie['Genre'] }}</p>
-        <p> {{ $movie['Director'] }}</p>
-        <p> {{ $movie['Actors'] }}</p>
-        <p> {{ $movie['Plot'] }}</p>
-        <p> {{ $movie['imdbRating'] }}</p>
+        <p><strong>Year:</strong> {{ $movie['Year'] }}</p>
+        <p><strong>Rated:</strong> {{ $movie['Rated'] }}</p>
+        <p><strong>Released:</strong> {{ $movie['Released'] }}</p>
+        <p><strong>Runtime:</strong> {{ $movie['Runtime'] }}</p>
+        <p><strong>Genre:</strong> {{ $movie['Genre'] }}</p>
+        <p><strong>Director:</strong> {{ $movie['Director'] }}</p>
+        <p><strong>Actors:</strong> {{ $movie['Actors'] }}</p>
+        <p><strong>Plot:</strong> {{ $movie['Plot'] }}</p>
+        <p><strong>IMDB Rating:</strong> {{ $movie['imdbRating'] }}</p>
 
-        
-       <img src="{{ $movie['Poster'] }}" alt="Movie Poster" class="mt-4">
-     
-   
+        @if ($movie['Poster'] !== 'N/A')
+            <img src="{{ $movie['Poster'] }}" alt="Movie Poster" class="mt-4">
+        @endif
+    @else
+        <p>Movie details not found.</p>
     @endif
 
-
+    <a href="/" class="text-blue-500 hover:underline mt-4 block">Back to Search</a>
 </div>
