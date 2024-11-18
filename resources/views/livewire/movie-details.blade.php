8d143eb64f6e1f92b2af05c02e3bac59f7b459be
Tiyiselani Mabunda <tiyiselaadvo@gmail.com>
2024-11-18 04:09:09 +0200

    Update movie-details.blade.php

--- resources/views/livewire/movie-details.blade.php
+++ resources/views/livewire/movie-details.blade.php
@@ -1,20 +1,28 @@
-<div class="container mx-auto p-6">
-    @if ($movie)
-        <h1 class="text-2xl font-bold">{{ $movie['Title'] }}</h1>
-        <p><strong>Year:</strong> {{ $movie['Year'] }}</p>
-        <p><strong>Rated:</strong> {{ $movie['Rated'] }}</p>
-        <p><strong>Released:</strong> {{ $movie['Released'] }}</p>
-        <p><strong>Runtime:</strong> {{ $movie['Runtime'] }}</p>
-        <p><strong>Genre:</strong> {{ $movie['Genre'] }}</p>
-        <p><strong>Director:</strong> {{ $movie['Director'] }}</p>
-        <p><strong>Actors:</strong> {{ $movie['Actors'] }}</p>
-        <p><strong>Plot:</strong> {{ $movie['Plot'] }}</p>
-        <p><strong>IMDB Rating:</strong> {{ $movie['imdbRating'] }}</p>
+<div class=" max-w-6xl mx-auto p-6">
+    <div class="grid sm:grid-cols-2 gap-5">
 
-        @if ($movie['Poster'] !== 'N/A')
-            <img src="{{ $movie['Poster'] }}" alt="Movie Poster" class="mt-4">
+        @if ($movie)
+            <div class="">
+                <h1 class="text-2xl font-bold">{{ $movie['Title'] }}</h1>
+                <p><strong>Year:</strong> {{ $movie['Year'] }}</p>
+                <p><strong>Rated:</strong> {{ $movie['Rated'] }}</p>
+                <p><strong>Released:</strong> {{ $movie['Released'] }}</p>
+                <p><strong>Runtime:</strong> {{ $movie['Runtime'] }}</p>
+                <p><strong>Genre:</strong> {{ $movie['Genre'] }}</p>
+                <p><strong>Director:</strong> {{ $movie['Director'] }}</p>
+                <p><strong>Actors:</strong> {{ $movie['Actors'] }}</p>
+                <p><strong>Plot:</strong> {{ $movie['Plot'] }}</p>
+                <p><strong>IMDB Rating:</strong> {{ $movie['imdbRating'] }}</p>
+            </div>
+            <div class="">
+                @if ($movie['Poster'] !== 'N/A')
+                    <img src="{{ $movie['Poster'] }}" alt="Movie Poster" class="mt-4">
+                @endif
+
+            </div>
         @endif
-    @else
+    </div>
+    @if (!$movie)
         <p>Movie details not found.</p>
     @endif
 
